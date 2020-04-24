938a776c0caa755ec268031a3df43989844bc1cf
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-04-24 15:59:10 +0200

    Upgrade: Change tpbtools and TIC para Bien references with Ayuda Digital

--- devcontrol/global/startup.sh
+++ devcontrol/global/startup.sh
@@ -2,5 +2,5 @@
 
 # @file devcontrol/global/startup.sh
 # @brief devcontrol startup script and functions
-echo "Dockerized Cypress Crawler (c) 2020 TIC para Bien"
+echo "Dockerized Cypress Crawler (c) 2020 Ayuda Digital"
 echo
